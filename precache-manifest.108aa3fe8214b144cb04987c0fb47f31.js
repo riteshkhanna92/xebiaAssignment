7948d51b8590a0db1d194fe2e60747ef9787c005
ritesh khanna <riteshkhanna92>
2019-07-18 21:21:11 +0530

    Updates

---
+++ precache-manifest.108aa3fe8214b144cb04987c0fb47f31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97ab236bee602b59c4823cc7b835b228",
+    "url": "/xebiaAssignment/index.html"
+  },
+  {
+    "revision": "ce64276a72e2047cf8c8",
+    "url": "/xebiaAssignment/static/css/main.bd76f0bc.chunk.css"
+  },
+  {
+    "revision": "0f0c93f35eb93dc0b294",
+    "url": "/xebiaAssignment/static/js/2.856b7c56.chunk.js"
+  },
+  {
+    "revision": "ce64276a72e2047cf8c8",
+    "url": "/xebiaAssignment/static/js/main.d6189774.chunk.js"
+  },
+  {
+    "revision": "f8dd249e8f8c955db0fe",
+    "url": "/xebiaAssignment/static/js/runtime~main.f48e18d7.js"
+  }
+]);
